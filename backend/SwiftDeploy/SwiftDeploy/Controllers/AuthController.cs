9f1061df57b4a35c77b48c0a6d319daa7a80679a
Muskan-AK-3130 <muskanajain30@gmail.com>
2025-07-31 15:42:10 +0530

    GitHub O Auth test

---
+++ backend/SwiftDeploy/SwiftDeploy/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+
+
+namespace SwiftDeploy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("github/login")]
+        public IActionResult GitHubLogin()
+        {
+           return Challenge(new AuthenticationProperties { RedirectUri = "/" }, "GitHub");
+        }
+
+            [HttpGet("github/callback")]
+            public async Task<IActionResult> GitHubCallback()
+            {
+                var result = await HttpContext.AuthenticateAsync();
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+                return Ok(new
+                {
+                    token = accessToken,
+                    username = result.Principal.Identity.Name
+                });
+            }
+        }
+    
+}
